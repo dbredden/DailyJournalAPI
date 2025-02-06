6dfc4e91c07da107bd72a98b9d118ca1aca0e6c3
Danny Redden <dredden@allegion.com>
2025-02-06 16:27:27 -0500

    added project references and di setup

--- API/Program.cs
+++ API/Program.cs
@@ -14,6 +14,8 @@ namespace API
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+            builder.Services.AddAppDI();
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
