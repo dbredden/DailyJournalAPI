42f622cd43163d381d458eaf585ff4ca67ba2898
Danny Redden <dannyredden18@gmail.com>
2025-02-12 23:07:12 -0500

    added interface, mediatr package and repository functions

---
+++ Domain/Interfaces/IJournalRepository.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities;
+
+namespace Domain.Interfaces;
+
+public interface IJournalRepository
+{
+    Task<IEnumerable<JournalEntity>> GetJournals();
+    Task<JournalEntity> GetJournalsByIdAsync(Guid id);
+    Task<JournalEntity> AddJournalAsync(JournalEntity entity);
+    Task<JournalEntity> UpdateJournalAsync(Guid journalId, JournalEntity entity);
+    Task<bool> DeleteJournalAsync(Guid journalId);
+}
