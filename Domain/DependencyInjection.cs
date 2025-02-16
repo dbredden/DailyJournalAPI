dc6c4484dd5f75fb2915a67147c729823d58e9ea
Danny Redden <dannyredden18@gmail.com>
2025-02-16 16:59:10 -0500

    added options pattern

--- Domain/DependencyInjection.cs
+++ Domain/DependencyInjection.cs
@@ -1,16 +1,15 @@
-﻿using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Domain.Options;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Domain
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddDomainDI(this IServiceCollection services)
+        public static IServiceCollection AddDomainDI(this IServiceCollection services, IConfiguration configuration)
         {
+            services.Configure<ConnectionStringOptions>(configuration.GetSection(ConnectionStringOptions.SectionName));
+
             return services;
         }
     }
